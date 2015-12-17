7a97bf4d83a2b62bd53881057ede94f78a7fd107
Daniel Langdon <me@danielsadventure.info>
2015-12-16 20:50:33 -0700

    Release 0.1

---
+++ ComputationModule.js
@@ -0,0 +1,281 @@
+﻿// This code is not read with a script tag.  Instead, it is imported with an AJAX call and then the string
+// DO_COMPUTATION is replaced with the code for a doComputation function.  eval() is then used to compute
+// the programming that actually runs.  This is because asm.js does not allow for its programming to be
+// modified at run-time.
+// The use of eval would probably give Douglas Crockford heart palpitations :-)
+// Note that the functions that implement the operators, trig functions, logarithms, etc
+// return no value, but rather save the real part in the outR and the imaginary part in the outI variables.
+// When the computation is all done, use get_outR and get_outI to get the results.
+
+function getComputationModule() {
+    var computationModule = (function foo1(stdlib, foreign, heap) {
+        "use asm";
+        var sqrt = stdlib.Math.sqrt,
+            sin = stdlib.Math.sin,
+            cos = stdlib.Math.cos,
+            atan = stdlib.Math.atan,
+            atan2 = stdlib.Math.atan2,
+            exp = stdlib.Math.exp,
+            ln = stdlib.Math.log,
+            floor = stdlib.Math.floor,
+            ceil = stdlib.Math.ceil,
+            int32Array = new stdlib.Int32Array(heap),
+            float64Array = new stdlib.Float64Array(heap),
+            outR = 0.0,
+            outI = 0.0;
+                        
+        "DO_COMPUTATION"        
+
+        // Truncates the decimal part of a real number.
+        function truncateDecimal(r) {
+            r = +r;
+
+            if (+r > 0.0)
+                return +floor(r);
+            else
+                return +ceil(r);
+            return 0.0;
+        }
+
+        // Compute the result of [r,i] raised to the power with the given real and imaginary parts..        
+        // Places the resulting real part in outR and the imaginary part in outI.
+        function computePower(r, i, expr, expi) {
+            // Tell asm.js that r, i, expr, and expi are floating-point numbers.
+            r = +r;
+            i = +i;
+            expr = +expr;
+            expi = +expi;
+
+            // Declare and initialize variables to be numbers.
+            var rResult = 0.0;
+            var iResult = 0.0;
+            var j = 0.0;
+            var tr = 0.0;
+            var ti = 0.0;
+
+            // Declare and initialize variables that will be used only in the
+            // event we need to compute the reciprocal.
+            var abs_squared = 0.0;
+            var recr = 0.0;
+            var reci = 0.0;
+
+            if (+truncateDecimal(expr) == +expr) if (expi == 0.0) {
+                // Compute the result if the exponent is an integer real number.
+                if (+expr < 0.0) {
+                    // For n less than 0, compute the reciprocal and then raise it to the opposite power.
+                    abs_squared = +(r * r + i * i);
+                    recr = +r / abs_squared;
+                    reci = -i / abs_squared;
+                    r = recr;
+                    i = reci;
+                    expr = +(-expr);
+                }
+
+                rResult = r;
+                iResult = i;
+
+                for (j = 1.0; +j < +expr; j = +(j + 1.0)) {
+                    tr = rResult * r - iResult * i;
+                    ti = rResult * i + iResult * r;
+                    rResult = tr;
+                    iResult = ti;
+                }
+
+                outR = rResult;
+                outI = iResult;
+                return;
+            }
+             
+            // If the exponent is not a whole number or has non-zero imaginary part, use logarithms
+            // together with the exponential function to compute the power.
+            // x ^ y = e ^ (ln(x) * y)
+
+            // Compute the natural log of the base:
+            compute_ln(r, i);
+
+            // Multiply that by the exponent:
+            multiply(outR, outI, expr, expi);
+
+            // Exponentiate the result
+            compute_exp(outR, outI);
+
+            // The result is now in outR, outI.            
+        } // end computePower
+
+        function add(r0, i0, r1, i1) {
+            r0 = +r0;
+            i0 = +i0;
+            r1 = +r1;
+            i1 = +i1;
+
+            outR = +(r0 + r1);
+            outI = +(i0 + i1);
+        }
+
+        function subtract(r0, i0, r1, i1) {
+            r0 = +r0;
+            i0 = +i0;
+            r1 = +r1;
+            i1 = +i1;
+
+            outR = +(r0 - r1);
+            outI = +(i0 - i1);
+        }
+
+        function multiply(r0, i0, r1, i1) {
+            r0 = +r0;
+            i0 = +i0;
+            r1 = +r1;
+            i1 = +i1;
+
+            outR = r0 * r1 - i0 * i1;
+            outI = r0 * i1 + r1 * i0;
+        }
+
+        function divide(r0, i0, r1, i1) {
+            r0 = +r0;
+            i0 = +i0;
+            r1 = +r1;
+            i1 = +i1;
+
+            outR = +(((r0 * r1) + (i0 * i1)) / (r1 * r1 + i1 * i1));
+            outI = +(((i0 * r1 - r0 * i1)) / (r1 * r1 + i1 * i1));
+        }
+
+        function compute_real(r, i) {
+            r = +r;
+            i = +i;
+            outR = +r;
+            outI = 0.0;
+        }
+
+        function compute_imag(r, i) {
+            r = +r;
+            i = +i;
+            outR = 0.0;
+            outI = +i;
+        }
+
+        function compute_abs(r, i) {
+            r = +r;
+            i = +i;
+
+            // If the number is purely real, no need to compute square roots.                        
+            if (i == 0.0) {
+                outR = +(+r > 0.0 ? +r : -r);
+                outI = 0.0;
+            } else {
+                outR = +sqrt(r * r + i * i);
+                outI = 0.0;
+            }
+        }
+
+        // Compute the "Argument" of a complex number, that is the angle of the number in polar coordinates.
+        function compute_arg(r, i) {
+            r = +r;
+            i = +i;
+            if (r == 0.0 & i == 0.0) {
+                // Although arg(0) is undefined, I will use 0 here to avoid errors.
+                outR = 0.0;
+                outI = 0.0;
+            }
+            else {
+                // outR = +(2.0 * +atan(i / (+sqrt(r * r + i * i) + r)));
+                outR = +(atan2(i, r));
+                outI = 0.0;
+            }
+        }
+
+        // Compute the conjugate of a complex number.
+        function compute_conj(r, i) {
+            r = +r;
+            i = +i;
+            outR = +r;
+            outI = +(-i);
+        }
+
+        // Compute the sine of a number given its real and imaginary parts.
+        function compute_sin(r, i) {
+            r = +r;
+            i = +i;
+            outR = +(+sin(r) * (+exp(i) + +exp(-i)) / +2);
+            outI = +(+cos(r) * (+exp(i) - +exp(-i)) / +2);
+        }
+
+        // Compute hyperbolic sine using the formula below.
+        // sinh(x) = -i * sin(i * x)
+        function compute_sh(r, i) {
+            r = +r;
+            i = +i;            
+            multiply(r, i, 0.0, 1.0);
+            compute_sin(outR, outI);
+            multiply(outR, outI, 0.0, -1.0);
+        }
+
+        function compute_cos(r, i) {
+            r = +r;
+            i = +i;
+            outR = +(+cos(r) * (+exp(i) + +exp(-i)) / +2);
+            outI = +(-(+sin(r)) * (+exp(i) - +exp(-i)) / +2);
+        }
+        
+        // Compute the hyperbolic cosine using the formula below.
+        // cosh(x) = cos(i * x)
+        function compute_ch(r, i) {
+            r = +r;
+            i = +i;            
+            multiply(r, i, 0.0, 1.0);
+            compute_cos(outR, outI);
+        }
+
+        // Compute the natural exponental for a number given its real and imaginary parts.
+        function compute_exp(r, i) {
+            r = +r;
+            i = +i;
+            var t = 0.0;
+            t = +exp(+r);
+            outR = +(t * +cos(i));
+            outI = +(t * +sin(i));
+        }
+
+        // Compute the natural log for a number given its real and imaginary parts.
+        // ln(a+bi) = ln(abs(z)) + i * arg(z)
+        function compute_ln(r, i) {
+            r = +r;
+            i = +i;
+            var realPart = 0.0,
+                imagPart = 0.0;
+            compute_abs(r, i);
+            realPart = +ln(outR);
+            compute_arg(r, i);
+            imagPart = +outR;
+            outR = +realPart;
+            outI = +imagPart;
+        }
+
+        function get_outR() {
+            return +outR;
+        }
+        function set_outR(r) {
+            r = +r;
+            outR = +r;
+        }
+
+        function get_outI() {
+            return +outI;
+        }
+        function set_outI(i) {
+            i = +i;
+            outI = +i;
+        }
+
+        return {
+            doComputation: doComputation,            
+            get_outR: get_outR,            
+            get_outI: get_outI,            
+        };
+    })(self, foreign, heap);
+
+    // Return computationModule that we just defined.
+    return computationModule;
+}
---
+++ ConvertSvc.js
@@ -0,0 +1,165 @@
+﻿// ConvertSvc.js
+// Provide functionality to convert a text math expression, such as 3 + ln(5 + 2*i) to the appropriate asm.js code.
+// Think of it as a transpiler that converts conventional mathematical notation to asm.js code.
+// jslint directive
+/*jslint browser: true, white: true, for: true*/
+
+var ConvertSvc = [function () {
+    'use strict';
+    var that = this,
+        // Note that these operators are sorted by precedence from lowest to highest.
+        OPERATORS = ['-', '+', '/', '*', '^'],
+        // These match the names of the functions to compute the operators in ComputationModule.js.
+        OPERATOR_NAMES = ['subtract', 'add', 'divide', 'multiply', 'computePower'],
+        // When compute_ is prepended to these names, it matches the names of the functions to compute them in ComputationModule.js.
+        ONE_ARG_FUNCTIONS = ['real', 'imag', 'abs', 'arg', 'sin', 'cos', 'sh', 'ch', 'exp', 'ln', 'conj'];        
+
+    // Parse the input into a sort of tree.
+    // Represent the input as an array.  The first element of the array is an operator or function.
+    // If the first element is an operator, it is followed by two arguments, which may themselves contain more mathematics.
+    // If it is a function, it is followed by one argument.    
+    // This function operates recursively.
+    function parseTextToMathArray(text) {
+        function recurse(text) {
+            var i, index, operand0, operand1;
+
+            // Search for the substring within the given text.
+            // Return the index of the first letter of the first instance of the substring or -1 if it isn't found.
+            // This is used to search for operators, function names, and parenthesis.
+            function findIndex(toFind, text) {
+                var j, c, numParens = 0;
+                // numParents keeps track of how deep inside parenthesis we are nested; we only want to match outside the parenthesis.
+                for (j = 0; j < text.length; j += 1) {
+                    c = text.substr(j, toFind.length);
+                    // The first part of the expression checks for a match.
+                    // The second part of the expression checks that we're not looking for subtraction and finding a unary '-' instead.
+                    if (c === toFind && numParens === 0 && !(toFind === '-' && (j === 0 || OPERATORS.indexOf(text[j - 1] > -1)))) {
+                        return j;
+                    }
+                    if (c.substr(0, 1) === '(') {
+                        numParens += 1;
+                    } else if (c.substr(0, 1) === ')') {
+                        numParens -= 1;
+                    }
+                }
+                return -1;
+            } // end function findIndex
+
+            // Search for each of our -+/*^ operators.
+            // They are sorted by precedence from lowest to highest.
+            for (i = 0; i < OPERATORS.length; i += 1) {                
+                index = findIndex(OPERATORS[i], text);
+                if (index > -1) {
+                    operand0 = text.substr(0, index);
+                    operand1 = text.substr(index + 1);
+                    return [OPERATORS[i], recurse(operand0), recurse(operand1)];
+                }
+            }            
+            // If we didn't find any of the operators, look for the one argument functions
+            for (i = 0; i < ONE_ARG_FUNCTIONS.length; i += 1) {
+                index = findIndex(ONE_ARG_FUNCTIONS[i], text);
+                if (index > -1) {
+                    operand0 = text.substr(index + ONE_ARG_FUNCTIONS[i].length, text.length - ONE_ARG_FUNCTIONS[i].length - index * 2);
+                    return [ONE_ARG_FUNCTIONS[i], recurse(operand0)];
+                }
+            }
+            // If we've made it this far, there isn't a single operation we can perform without going inside parenthesis.
+            // If there are parenthesis, recurse on what's inside the parenthesis.
+            index = findIndex('(', text);
+            if (index === 0) {
+                return recurse(text.substr(1, text.length - 2));
+            }
+            // If we've made it this far, this is a primitive expression, so return it without any further processing; recursion ends here.
+            return text;
+        } // end function recurse        
+
+        // Remove spaces, replace long dash with short one, and convert to lower case.  This happens only once before recursion begins.
+        text = text.replace(/ /g, '');        
+        text = text.replace(/−/g, '-');
+        text = text.toLowerCase();
+                        
+        return recurse(text);
+    } // end function parseTextToMathArray
+
+    // Parse a tree-like structure representing a mathematical expression and generate corresponding asm.js code.    
+    // This function operates recursively.
+    function convertMathArrayToAsmjs(mathArray) {
+        var i, numVariables,variableDeclarations, computationCode, asmjsCode;
+
+        function recurse(mathArray) {
+            var firstVariableNum, secondVariableNum, result = [];
+            
+            // TODO: check for a literal with i
+            if (typeof mathArray === 'string') {
+                // If we have a string instead of an array, this should be a number or a variable (z or c)
+                if (!isNaN(mathArray)) {
+                    result.push('outR = +' + mathArray + ';\noutI = +0;\n');
+                } else if (mathArray === 'i') {
+                    result.push('outR = 0.0;\noutI = 1.0;\n');
+                } else if (mathArray === '-i') {
+                    result.push('outR = 0.0;\noutI = -1.0;\n');
+                }
+            } else if (OPERATORS.indexOf(mathArray[0]) > -1) {
+                // var var_n;
+                firstVariableNum = numVariables;
+                numVariables += 1;
+                // Result will be in outR and outI
+                result.push(recurse(mathArray[1]));
+                result.push('__r' + firstVariableNum + ' = +outR;\n');
+                result.push('__i' + firstVariableNum + ' = +outI;\n');
+
+                secondVariableNum = numVariables;
+                numVariables += 1;
+                result.push(recurse(mathArray[2]));
+                result.push('__r' + secondVariableNum + ' = +outR;\n');
+                result.push('__i' + secondVariableNum + ' = +outI;\n');                
+
+                for (i = 0; i < OPERATORS.length; i += 1) {
+                    if (mathArray[0] === OPERATORS[i]) {
+                        result.push(OPERATOR_NAMES[i] + '(__r' + firstVariableNum + ', __i' + firstVariableNum + ', __r' + secondVariableNum + ', __i' + secondVariableNum + ');\n');
+                        break;
+                    }
+                }                
+            } else if (ONE_ARG_FUNCTIONS.indexOf(mathArray[0]) > -1) {
+                // Single argument functions
+                firstVariableNum = numVariables;
+                numVariables += 1;
+                result.push(recurse(mathArray[1]));
+                result.push('__r' + firstVariableNum + ' = outR;\n');
+                result.push('__i' + firstVariableNum + ' = outI;\n');
+
+                // This assumes that the function to compute this in asm.js is named 'compute_' followed by the name of the function.
+                result.push('compute_' + mathArray[0] + '(__r' + firstVariableNum + ', __i' + firstVariableNum + ');\n');
+            }
+
+            return result.join('');
+        }
+
+        numVariables = 0;
+
+        // numVariables will be incremented to the actual number of variables to declare when recurse is called.
+        computationCode = recurse(mathArray);
+
+        // Declare all the the asm.js variables as doubles.
+        variableDeclarations = [];
+        for (i = 0; i < numVariables; i += 1) {
+            variableDeclarations.push('var __r' + i + ' = 0.0;\n');
+            variableDeclarations.push('var __i' + i + ' = 0.0;\n');
+        }
+        variableDeclarations = variableDeclarations.join('');
+
+        asmjsCode = variableDeclarations + computationCode;        
+        return asmjsCode;
+    }
+
+    // Convert a textual mathematics expression to asm.js code.
+    // This is the only function that is exposed to the calling code.
+    function convert(text) {
+        var mathArray, asmjsCode;
+        mathArray = parseTextToMathArray(text);
+        asmjsCode = convertMathArrayToAsmjs(mathArray);
+        return asmjsCode;
+    }
+
+    that.convert = convert;
+}];
